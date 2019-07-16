a9e00c548dc8eae629244ab0f8a0849cf871266a
pwachira900@gmail.com <pwachira900@gmail.com>
2019-07-16 19:11:01 +0300

    add login and user registration activity in the UI package

---
+++ app/src/main/java/com/peterwachira/wazinsureapiclient/UI/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.peterwachira.wazinsureapiclient.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.peterwachira.wazinsureapiclient.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
